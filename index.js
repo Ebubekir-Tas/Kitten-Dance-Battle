8ed2e3ad95df29f059189d4e9c5d38b66600e9ee
Ebubekir Tas <Tas_Ebubekir@yahoo.com>
2020-07-12 23:56:57 -0400

    Added winner header and reset timer

--- index.js
+++ index.js
@@ -1,50 +1,78 @@
 //When I press Z I want #stick1 src to be "images/stick1.png". When I press X I want #stick1 src to be "images/stick2.png"
 
 
-//Animation Stick 1
-document.addEventListener("keydown", function(e){
-    if(e.code == "KeyZ"){
-        $('#stick1').attr("src", "images/stick1.png");  
-    }
-    })
 
-document.addEventListener("keydown", function(e){
-    if(e.code == "KeyX"){
-        $('#stick1').attr("src", "images/stick2.png");
-        P1ScoreX = Math.floor(Math.random() * 9)+1; //Random number from 1-10
-        return P1ScoreX;
-    }
-    }) 
+//Animation Stick 1
+document.addEventListener("keydown", function (e) {
+  if (e.code == "KeyZ") {
+    $("#stick1").attr("src", "images/stick1.png");
+  }
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.code == "KeyX") {
+    $("#stick1").attr("src", "images/stick2.png");
+    P1Score += Math.floor(Math.random() * 9) + 1; //Random number from 1-10
+    return P1Score;
+  }
+});
 
 //Animation Stick 2
-document.addEventListener("keydown", function(f){
-    if(f.code == "KeyN"){
-        $('#stick2').attr("src", "images/stick2.png");
+document.addEventListener("keydown", function (f) {
+  if (f.code == "KeyN") {
+    $("#stick2").attr("src", "images/stick2.png");
+  }
+});
+
+document.addEventListener("keydown", function (f) {
+  if (f.code == "KeyM") {
+    $("#stick2").attr("src", "images/stick1.png");
+  }
+});
+
+//Stick 1 Score and Winner
+
+let P1Score = 0;
+
+
+document.addEventListener("keydown", function (e) {
+  if (e.code == "KeyZ") {
+    P1Score += Math.floor(Math.random() * 9) + 1; //Random number from 1-10
+    console.log(P1Score)
+  }
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.code == "KeyX") {
+    P1Score += Math.floor(Math.random() * 9) + 1; //Random number from 1-10
+    console.log(P1Score);
+    if (P1Score >= 300){
+        $('h2').text('Player 1 wins!')
+        setTimeout(function(){
+            window.location.reload(true);}, 2000)
     }
-    })
+  }
+});
 
-document.addEventListener("keydown", function(f){
-    if(f.code == "KeyM"){
-        $('#stick2').attr("src", "images/stick1.png");
+//Stick 2 Score and Winner
+
+let P2Score = 0;
+
+document.addEventListener("keydown", function (f) {
+    if (f.code == "KeyN") {
+      P2Score += Math.floor(Math.random() * 9) + 1; //Random number from 1-10
+      console.log(P2Score)
     }
-    })
-
-//Stick 1 Score
-
-
-document.addEventListener("keydown", function(e){
-    if(e.code == "KeyZ"){
-        P1ScoreZ = Math.floor(Math.random() * 9)+1; //Random number from 1-10
-    }   
-    })
-
-document.addEventListener("keydown", function(e){
-    if(e.code == "KeyX"){
-        P1ScoreX = Math.floor(Math.random() * 9)+1; //Random number from 1-10
-        
+  });
+  
+  document.addEventListener("keydown", function (f) {
+    if (f.code == "KeyM") {
+      P2Score += Math.floor(Math.random() * 9) + 1; //Random number from 1-10
+      console.log(P2Score);
+      if (P2Score >= 300){
+        $('h2').text('Player 2 wins!')
+            setTimeout(function(){
+          window.location.reload(true);}, 2000)
+      }
     }
-    })
-
-
-
-
+  });
