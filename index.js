dfc8c6dab252b123308c1ed4a6695dd0826547ae
Ebubekir Tas <Tas_Ebubekir@yahoo.com>
2020-07-26 20:17:45 -0400

    Color scheme, images, fonts

--- index.js
+++ index.js
@@ -11,19 +11,20 @@ var scoreCap = 100;
 
 var increment = 9; //9 by default, can be changed in browser through Game Settings tab.
 
-var refreshTimer = 2000 //2000 be default, can be changed in browser through Game Settings tab.
+var refreshTimer = 3000 //3 seconds
 
 function stop(){if ($("h2").not(':empty')){$('body').on("keydown", false)}}        //Winner announcement = Disable keyboard inputs)
                                                                                   
 function refresh(){window.location.reload(true);}                                //refresh
                                                                                 
-function randomScore1(){p1Score += Math.floor(Math.random() * increment) + 1;}         //Player 1 random score generator (Must be different from Player 2)
-                                                                              
+function randomScore1(){p1Score += Math.floor(Math.random() * increment) + 1;}         //Player 1 random score generator (Must be different from Player 2)                                                                 
 function randomScore2(){p2Score += Math.floor(Math.random() * increment) + 1;}       //Player 2 random score generator (Must be different from Player 1)
                                                                             
-function p1Wins(){$('h2').css("color", "#fff").text('Player 1 wins!')}     //Player 1 Winner Declaraion
-                                                                          
+function p1Wins(){$('h2').css("color", "#fff").text('Player 1 wins!')}     //Player 1 Winner Declaraion                                                             
 function p2Wins(){$('h2').css("color", "#fff").text('Player 2 wins!')}   //Player 2 Winner Declaraion
+
+function p1Flash(){$('#player1').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)}
+function p2Flash(){$('#player2').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)}
 
 
 //Audio
@@ -41,8 +42,8 @@ function play() {
 
 //Press the Z key                                                      
 document.addEventListener("keydown", function (e) {                   
-  if (e.code == "KeyZ" && $("img#player1").attr("src") == "images/stick2.png"){    //If you press Z
-    $("#player1").attr("src", "images/stick1.png");                  //The image swaps to stick1 (arms down)
+  if (e.code == "KeyZ" && $("img#player1").attr("src") == "images/Cat2.png"){    //If you press Z
+    $("#player1").attr("src", "images/Cat1.png");                  //The image swaps to Cat1
     randomScore1();  
     play();                                               
     console.log(p1Score);                           
@@ -52,7 +53,7 @@ document.addEventListener("keydown", function (e) {
           setTimeout(function(){                              
         refresh()}, refreshTimer)                                    
         stop();                                            
-        $('#player1').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+        p1Flash();
       }
   }
 });
@@ -60,8 +61,8 @@ document.addEventListener("keydown", function (e) {
 
 //Press the X key
 document.addEventListener("keydown", function (e) {
-  if (e.code == "KeyX" && $("img#player1").attr("src") == "images/stick1.png"){    //If you press X
-    $("#player1").attr("src", "images/stick2.png");                 //The image swaps to stick2 (arms up)
+  if (e.code == "KeyX" && $("img#player1").attr("src") == "images/Cat1.png"){    //If you press X
+    $("#player1").attr("src", "images/Cat2.png");                 //The image swaps to Cat2
     randomScore1();     
     play();                                           
     console.log(p1Score);
@@ -71,7 +72,7 @@ document.addEventListener("keydown", function (e) {
           setTimeout(function(){
         refresh()}, refreshTimer)                                   
         stop();                                           
-        $('#player1').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+        p1Flash();
       }
   }
 });
@@ -79,8 +80,8 @@ document.addEventListener("keydown", function (e) {
 
 //Press the N key
 document.addEventListener("keydown", function (f) {
-  if (f.code == "KeyN" && $("img#player2").attr("src") == "images/playerdos1.png"){    //If you press N
-    $("#player2").attr("src", "images/playerdos2.png");                 //The image swaps to player 2 (arms up)
+  if (f.code == "KeyN" && $("img#player2").attr("src") == "images/SecondCat1.png"){    //If you press N
+    $("#player2").attr("src", "images/SecondCat2.png");                 //The image swaps to Second Cat 2
     randomScore2(); 
     play();                                               
     console.log(p2Score);                                        
@@ -90,7 +91,7 @@ document.addEventListener("keydown", function (f) {
           setTimeout(function(){                             
         refresh()}, refreshTimer)                                   
         stop();                                           
-        $('#player2').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+        p2Flash();
     }
   }
 });
@@ -98,8 +99,8 @@ document.addEventListener("keydown", function (f) {
 
 //Press the M key
 document.addEventListener("keydown", function (f) {
-  if (f.code == "KeyM" && $("img#player2").attr("src") == "images/playerdos2.png"){    //If you press M
-    $("#player2").attr("src", "images/playerdos1.png");                 //The image swaps to player 1 (arms down)
+  if (f.code == "KeyM" && $("img#player2").attr("src") == "images/SecondCat2.png"){    //If you press M
+    $("#player2").attr("src", "images/SecondCat1.png");                 //The image swaps to Second Cat 2
     randomScore2();                                               
     console.log(p2Score);
     $('#xpbar2').html('Player 2 Score: ' + p2Score + '/' + scoreCap)    
@@ -109,7 +110,7 @@ document.addEventListener("keydown", function (f) {
           setTimeout(function(){
         refresh()}, refreshTimer)                                   
         stop();                                            
-        $('#player2').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+        p2Flash();
     }
   }
 });
@@ -179,23 +180,3 @@ document.addEventListener("keydown", function (f) {
     increment = 49;
   })
 
-  //Dropdown Refresh Timer
-  $('.ref1').click(function(){
-    refreshTimer = 1000;
-  })
-
-  $('.ref2').click(function(){
-    refreshTimer = 2000;
-  })
-
-  $('.ref3').click(function(){
-    refreshTimer = 3000;
-  })
-
-  $('.ref4').click(function(){
-    refreshTimer = 4000;
-  })
-
-  $('.ref5').click(function(){
-    refreshTimer = 5000;
-  })
